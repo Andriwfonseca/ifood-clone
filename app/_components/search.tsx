5a09a7ae8d6a1a4d0130f77a1dcf06dfe5b2d5dd
Andriw Fonseca <filipefonsequinha@gmail.com>
2024-05-08 21:04:24 -0300

    feat: change styles input search component

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -4,8 +4,11 @@ import { Input } from "./ui/input";
 
 const Search = () => {
   return (
-    <div className="flex gap-1">
-      <Input placeholder="Buscar restaurantes" className="border-none" />
+    <div className="flex gap-2">
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+      />
       <Button size="icon">
         <SearchIcon size={20} />
       </Button>
