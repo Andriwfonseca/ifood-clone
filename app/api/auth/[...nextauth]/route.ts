a34dd97cb57f1b642284430a5e5f71e90a3f292d
Andriw Fonseca <filipefonsequinha@gmail.com>
2024-08-30 00:39:25 -0300

    feat: add orders page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,7 @@
-import { db } from "@/app/_lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth/next";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
 
-console.log(process.env.GOOGLE_CLIENT_ID, "process.env.GOOGLE_CLIENT_ID");
-console.log(
-  process.env.GOOGLE_CLIENT_SECRET,
-  "process.env.GOOGLE_CLIENT_SECRET",
-);
-const handler = NextAuth({
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user = { ...session.user, id: user.id };
-      return session;
-    },
-  },
-});
+import { authOptions } from "@/app/_lib/auth";
+import NextAuth from "next-auth/next";
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
